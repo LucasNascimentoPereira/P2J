019a444033058c6eab89f7c81864d62acb4633a2
neo-rabbit <diogo.m.guedes@ua.pt>
2025-10-29 15:59:27 +0000

    stuff i forgot to change

--- P2J/Assets/Scripts/Controls/PlayerController.cs
+++ P2J/Assets/Scripts/Controls/PlayerController.cs
@@ -65,7 +65,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (IsOnGround() && rb.linearVelocityY == 0f)
+        if (IsOnGround())
         {
             onGround = true;
             airDashCount = 0;
