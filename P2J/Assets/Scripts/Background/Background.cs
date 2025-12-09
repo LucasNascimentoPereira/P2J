6b3346589ef9db236e885d550b04598ca448aa23
LucasPereiraWork <154391806+LucasPereiraWork@users.noreply.github.com>
2025-12-09 16:51:31 +0000

    parallax

---
+++ P2J/Assets/Scripts/Background/Background.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private BackgroundDataAsset backgroundDataAsset;
+    [SerializeField] private List<GameObject> layers;
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        playerController = GameManager.Instance.PlayerController;
+    }
+
+    private void FixedUpdate()
+    {
+        //transform.position = playerTransform.position;
+        for (int i = 0; i < layers.Count; ++i) 
+        {
+            layers[i].transform.position = layers[i].transform.position + (backgroundDataAsset.MoveDelta * playerController.MoveValue.normalized);
+            layers[i].transform.position = Vector2.MoveTowards(layers[i].transform.position, layers[i].transform.position + (new Vector2(0, 0) * playerController.MoveValue.normalized), backgroundDataAsset.TimeInterval);
+
+        }
+    }
+
+}
