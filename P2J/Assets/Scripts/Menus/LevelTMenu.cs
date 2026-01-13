fc57793d03ac958daa897db770c43905f244c642
Lucas Nascimento Pereira <lucasnpereira@ua.pt>
2026-01-13 22:20:54 +0000

    menuuuus bugs

---
+++ P2J/Assets/Scripts/Menus/LevelTMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class LevelTMenu : MenusBaseState
+{
+	private float idleTime = 5.0f;	
+    public override void BeginState(UIManager uiManager)
+    {
+	    base.BeginState(uiManager);
+	    uiManager.BeginIdleTime(idleTime);
+    }
+    public override void UpdateState()
+    {
+	    base.UpdateState();
+    }
+    public override void ExitState()
+    {
+	    base.ExitState();
+	    uiManager.ShowPanelEnum(UIManager.menusState.HUD);
+    }
+}
